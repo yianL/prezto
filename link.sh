5e4d0a4fea1d62459f12cea1488f3f911daccece
Yi An Lai <hello@yian.dev>
2024-01-20 16:09:14 -0800

    fix link script

--- link.sh
+++ link.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
 setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.prezto/runcoms/^README.md(.N); do
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -sf "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
